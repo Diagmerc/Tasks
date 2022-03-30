088314131d497ba2fed4351bd3f19087cef027ca
Diagmerc <elozovo@gmail.com>
2022-03-30 13:12:22 +0300

    Фейсконтроль Напиши программу, которая считывает из консоли имя текстового файла, далее читает символы из этого файла (используй FileReader) и выводит на экран все, за исключением точки, запятой и пробела.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1526/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task15.task1526;
+
+import java.io.*;
+import java.util.Scanner;
+
+/* 
+Фейсконтроль
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in);
+             FileReader reader = new FileReader(scanner.nextLine())
+        ) {
+            while (reader.ready()) {
+                char ch = (char) reader.read();
+                if (ch != ' ' && ch != '.' && ch != ',') {
+                    System.out.print(ch);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Something went wrong" + e);
+        }
+    }
+}
